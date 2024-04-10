db90793c3602d8a70099f5e8cfe9a71411dba57e
Jorge Ribeiro <joorgemelo@gmail.com>
2024-04-10 11:37:00 +0400

    Update lambda logic execution

--- handler.py
+++ handler.py
@@ -1,51 +1,40 @@
-import time
+import logging
 import datetime
 import boto3
 
 from utils import Time
 from visa import VisaScheduler, Result
 
+logger = logging.getLogger("Visa_Logger")
+
 def lambda_handler(event, context):
     handler = VisaScheduler()
     result = handler.main()
+    logger.info(f'Lambda function executed with result: {result}')
     
-    next_execution_time, disable_rule = calculate_next_execution_time(result)
-    if not disable_rule:
-        update_eventbridge_rule_schedule(next_execution_time)
-    else:
-        disable_eventbridge_rule()
-
-def calculate_next_execution_time(result):
-    default_next_execution_time = datetime.datetime.now() + datetime.timedelta(seconds=Time.SUCCESS_TIME), False
+    disable_schedule = False
+    next_rate = Time.RETRY_TIME
     if result == Result.RETRY:
-        return datetime.datetime.now() + datetime.timedelta(seconds=Time.RETRY_TIME), False
+        next_rate = Time.RETRY_TIME
     elif result == Result.COOLDOWN:
-        return datetime.datetime.now() + datetime.timedelta(seconds=Time.COOLDOWN_TIME), False
+        next_rate = Time.COOLDOWN_TIME
     elif result == Result.EXCEPTION:
-        return datetime.datetime.now() + datetime.timedelta(seconds=Time.EXCEPTION_TIME), False
+        next_rate = Time.EXCEPTION_TIME
     elif result == Result.WEBDRIVER_EXCEPTION:
-        return datetime.datetime.now() + datetime.timedelta(seconds=Time.WEBDRIVER_EXCEPTION_TIME), False
+        next_rate = Time.WEBDRIVER_EXCEPTION_TIME
     elif result == Result.SUCCESS:
-        return datetime.datetime.now() + datetime.timedelta(seconds=Time.SUCCESS_TIME), True
+        next_rate = Time.SUCCESS_TIME
     elif result == Result.STOP:
-        return None, True
+        disable_schedule = True
     
-    return default_next_execution_time
-
-def update_eventbridge_rule_schedule(next_execution_time):
-    client = boto3.client('events')
-    rule_name = 'elizabethSlotSearchEventBridgeRule'
-
-    client.put_rule(
-        Name=rule_name,
-        ScheduleExpression=f'cron({next_execution_time.minute} {next_execution_time.hour} {next_execution_time.day} {next_execution_time.month} ? {next_execution_time.year})',
-        State='ENABLED'
-    )
-
-def disable_eventbridge_rule():
-    client = boto3.client('events')
-    rule_name = 'elizabethSlotSearchEventBridgeRule'
-
-    client.disable_rule(
-        Name=rule_name
+    event_arn = event["resources"][0]
+    event_arn = event_arn[event_arn.rindex("/") + 1:]
+    scheduler_client = boto3.client('scheduler')
+    schedule = scheduler_client.get_schedule(Name=event_arn)
+    scheduler_client.update_schedule(
+        FlexibleTimeWindow=schedule["FlexibleTimeWindow"], 
+        Name=event_arn, 
+        ScheduleExpression=f"rate({next_rate // 60} minutes)", 
+        Target=schedule["Target"], 
+        State="DISABLED" if disable_schedule else "ENABLED"
     )
