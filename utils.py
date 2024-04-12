670a61ff204477b87861f445cd21035d32833f7e
Jorge Ribeiro <joorgemelo@gmail.com>
2024-04-12 11:26:36 +0400

    Randomize retry times

--- utils.py
+++ utils.py
@@ -1,11 +1,12 @@
 from enum import Enum
+import random
 
 class Time:
-    RETRY_TIME = 60 * 10 # wait time when retrying: 10 minutes
-    COOLDOWN_TIME = 60 * 60  # wait time when temporary banned (empty list): 60 minutes
-    EXCEPTION_TIME = 60 * 5  # wait time when an exception occurs: 5 minutes
+    RETRY_TIME = random.choice([60 * 27, 60 * 34, 60 * 37])  # wait time when retrying: 27, 34 or 37 minutes
+    COOLDOWN_TIME = random.choice([60 * 123, 60 * 128, 60 * 131])  # wait time when temporarily banned (empty list): 123, 128 or 131 minutes
+    EXCEPTION_TIME = random.choice([60 * 6, 60 * 9, 60 * 13])  # wait time when an exception occurs: 6, 9 or 13 minutes
     WEBDRIVER_EXCEPTION_TIME = 60  # wait time when webdriver exception occurs: 1 minute
-    SUCCESS_TIME = 60 * 60 * 24 * 7  # wait time when success: 7 days
+    SUCCESS_TIME = 60 * 60 * 24 * 7  # wait time when successfully rescheduled: 7 days
 
 class Result(Enum):
     SUCCESS = 1
