43952563aa6ee556321eb73efe5d9fb3cf2cb2dd
Jorge Ribeiro <joorgemelo@gmail.com>
2025-03-03 17:24:31 +0400

    Update retry intervals

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ from enum import Enum
 import random
 
 class Time:
-    RETRY_TIME = random.choice([60 * 9, 60 * 11, 60 * 15])  # wait time when retrying: 9, 11 or 15 minutes
+    RETRY_TIME = random.choice([60 * 20, 60 * 24, 60 * 30])  # wait time when retrying: 20, 24 or 30 minutes
     COOLDOWN_TIME = random.choice([60 * 123, 60 * 128, 60 * 131])  # wait time when temporarily banned (empty list): 123, 128 or 131 minutes
     EXCEPTION_TIME = random.choice([60 * 6, 60 * 9, 60 * 13])  # wait time when an exception occurs: 6, 9 or 13 minutes
     WEBDRIVER_EXCEPTION_TIME = 60  # wait time when webdriver exception occurs: 1 minute
